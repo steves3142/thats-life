30b8c6355003750b6efa7706c3366e5378d13db3
steves3142 <steves3142@gmail.com>
2022-07-27 15:08:02 -0400

    adding back slash to getBankById and getCardById

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,7 +8,7 @@ router.get('/creditcards', controllers.getAllCards) //Creating a route on server
 router.get('/banks', controllers.getAllBanks) //Creating a route on server to connect the request with the controller 
 router.post('/banks', controllers.createBank)
 router.post('/creditcards', controllers.createCard)
-router.get('banks/:id', controllers.getBankById)
-router.get('creditcards/:id', controllers.getCardById)
+router.get('/banks/:id', controllers.getBankById)
+router.get('/creditcards/:id', controllers.getCardById)
 
 module.exports = router;
