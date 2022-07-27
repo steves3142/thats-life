3c3746458024efa4885809b24bc45b82a55d113f
steves3142 <steves3142@gmail.com>
2022-07-27 01:10:33 -0400

    Creating route on server for getting all Banks

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,5 +5,5 @@ const router = Router()
 router.get('/', (req, res) => res.send('This is root!'))
 
 router.get('/creditcards', controllers.getAllCards) //Creating a route on server to connect the request with the controller
-
+router.get('/banks', controllers.getAllBanks) //Creating a route on server to connect the request with the controller 
 module.exports = router;
