d9ebad43fdad847c0a5dc5d72f6b6690af0478b6
steves3142 <steves3142@gmail.com>
2022-07-27 19:40:27 -0400

    modified app.js

--- react/react-app/src/App.js
+++ react/react-app/src/App.js
@@ -2,31 +2,21 @@ import logo from './logo.svg'
 import './App.css'
 import React from 'react'
 import Header from './components/Header.js'
-import { Routes, Route } from 'react-router-dom'
-import ParkDetail from './components/ParkDetails'
-import Rides from './pages/Rides'
-import { useEffect, useState } from 'react'
 
 
 function App() {
+
+  console.log('hello')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <main>
+
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
