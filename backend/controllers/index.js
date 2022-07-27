fb077cf9673661f932a286b11edd818a131c379c
steves3142 <steves3142@gmail.com>
2022-07-27 12:19:28 -0400

    createBank and createCard with routes made

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,13 +1,37 @@
 const { Bank } = require('../models')
 const { Card } = require('../models')
 
+const createBank = async (req, res) => {
+    try {
+        const bank = await new Bank(req.body)
+        await bank.save()
+        return res.status(201).json({
+            bank,
+        }); 
+    } catch (error) {
+        throw error 
+    }
+}
+
+const createCard = async (req, res) => {
+    try {
+        const card  = await new Bank(req.body)
+        await card.save()
+        return res.status(201).json({
+            card,
+        })
+    } catch (error) {
+        throw error 
+    }
+}
+
 const getAllBanks = async (req, res) => {
   try {
     const banks = await Bank.find({})
     console.log(banks)
     return res.json({ banks })
   } catch (e) {
-    return res.send(e.message)
+    throw error
   }
 }
 
@@ -22,7 +46,11 @@ const getAllCards = async (req, res) => {
   }
 
 module.exports = {
+    createBank,
+    createCard,
     getAllBanks, 
     getAllCards
 }
 
+//200 good
+//404 file not found
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,4 +6,6 @@ router.get('/', (req, res) => res.send('This is root!'))
 
 router.get('/creditcards', controllers.getAllCards) //Creating a route on server to connect the request with the controller
 router.get('/banks', controllers.getAllBanks) //Creating a route on server to connect the request with the controller 
+router.post('/banks', controllers.createBank)
+router.post('/creditcards', controllers.createCard)
 module.exports = router;
