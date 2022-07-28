e7128a2bea9b1accd92c9f358850de727e1a7e4b
steves3142 <steves3142@gmail.com>
2022-07-28 17:50:56 -0400

    testing get credit card

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,3 +1,39 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+const arr = [1, 2, 3, 4, 5, 6]
+
+const Home = () => {
+  const [cards, setCreditCards] = useState([])
+
+  let navigate = useNavigate()
+
+  useEffect(() => {
+    console.log('helllo')
+    async function getCreditCards() {
+      console.log('helllo')
+      const res = await axios.get(`/api/creditcards/62e0b391406736594b044361`)
+      console.log(res)
+      setCreditCards(res.data.cards)
+    }
+    getCreditCards()
+  }, [])
+
+
+  return (
+    <div className="home">
+      {/* <div className="theme-box">
+        {cards.map((card, i) => (
+          <div>
+            <h1>{card.name}</h1>
+            <img src={card.image} alt="" />
+          </div>
+        ))}
+      </div> */}
+    </div>
+  )
+}
+
+export default Home
