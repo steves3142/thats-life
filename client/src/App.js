1240996cf981f500d78f296d4d8a5fd177b642d2
steves3142 <steves3142@gmail.com>
2022-07-28 10:50:30 -0400

    Testing browser page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg'
 import './App.css'
 import React from 'react'
 import Header from './components/Header.js'
+import Home from './components/Home.js'
 
 
 function App() {
@@ -10,10 +11,9 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
 
       <main>
-
+      <h2>That's Life</h2>
       </main>
     </div>
   )
