f88c22771ed640ddab60137961f5a9de7a855454
steves3142 <steves3142@gmail.com>
2022-07-29 15:08:53 -0400

    just kidding, now the bank menu is working

--- client/src/components/BankMenu.js
+++ client/src/components/BankMenu.js
@@ -5,34 +5,35 @@ import axios from 'axios'
 
 
 const BankMenu = () => {
-    const [banks, setBanks] = useState({})
+    const [banks, setBanks] = useState([]) //sqaure bracket for more than one bank curly for only one bank
   
     //let navigate = useNavigate()
   
     useEffect(() => {
-      console.log('hello')
-      async function getBanks() {
-        const res = await axios.get(`/api/banks`)
-        console.log(res)
-        let bankData = await res.data
-        console.log(bankData)
-        setBanks(bankData.banks)
-      }
-      getBanks()
-      console.log(banks)
-    }, [])
+    console.log('hello')
+    async function getBanks() {
+      const res = await axios.get(`/api/banks`)
+      console.log(res)
+      let bankData = await res.data
+      console.log(bankData)
+      setBanks(bankData.banks)
+    }
+    getBanks()
+    console.log(banks)
+  }, [])
+
     
     return (
       <><div className="bankmenu">
         <div>
-        <div className="bank-display-box">
-        {banks.map((bank, i) => (
+          <div className="bank-display-box">
+          {banks.map((bank, i) => (
           <div className="bank-info" key={i}>
             <h1>{bank.name}</h1>
             <img src={bank.image} alt="" />
             </div>
           ))}
-            </div>
+          </div>
         </div>
         </div></>
     )
