be2d697c415397154752419b39d2c9cd451120f5
steves3142 <steves3142@gmail.com>
2022-07-28 23:44:01 -0400

    Adding Navigation links to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <header>
+          <div>
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/bankmenu">Banking</NavLink>
+              </li>
+            </ul>
+          </div>
+    </header>
+
   )
 }
