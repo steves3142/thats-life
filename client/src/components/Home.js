978f310dbffd4fd5e32f6a956e2e5f2349683024
steves3142 <steves3142@gmail.com>
2022-07-31 20:30:27 -0400

    Setting up a Delete Card Form

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -61,43 +61,48 @@ const Home = () => {
       <h4>Add Your Credit Card</h4>
       <form onSubmit={ handleSubmit }>
 
-        <label htmlFor="name">Credit Card Name</label>
+        <label htmlFor="name">Credit Card Name: </label>
         <input type="text" id="name" onChange={handleChange} value={formState.name}/>
 
-        <label htmlFor="bank">Bank:</label>
+        <label htmlFor="bank">Bank: </label>
         <input type="text" id="bank" onChange={handleChange} value={formState.bank}/>
 
-        <label htmlFor="category">Category:</label>
+        <label htmlFor="category">Category: </label>
         <input type="text" id="category" onChange={handleChange} value={formState.category}/>
 
         <label htmlFor="image">Image Link:</label>
         <input type="text" id="image" onChange={handleChange} value={formState.image}/>
 
-        <label htmlFor="description">Description:</label>
+        <label htmlFor="description">Description: </label>
         <textarea id="description" cols="15" rows="3" onChange={handleChange} value={ formState.description }></textarea>
         <button type="submit">Submit</button>
 
       </form>
+      <h4>Delete A Card</h4>
+      <form onSubmit={ handleSubmit }>
+        <label htmlFor="name">Credit Card Name: </label>
+        <input type="text" id="name" onChange={handleChange} value={formState.name}/>
+        <button type="submit">Submit</button>
+      </form>
 
       <div>
-
-        <form>
+        {/* <form>
           <label htmlFor="issueType">Learn About:</label>
           <select id="issueType">
             <option value="cards">Credit Cards</option>
             <option value="banks">Banks</option>
           </select>
-        </form>
+        </form> */}
         <div className="card-display-box">
           {cards.map((card, i) => (
           <div className="credit-card" key={i}>
             <h1>{card.name}</h1>
             <img src={card.image} alt="" />
-            </div>
+          </div>
           ))}
-          </div>
-          </div>
-      </div></>
+        </div>
+      </div>
+    </div></>
   )
 }
 
